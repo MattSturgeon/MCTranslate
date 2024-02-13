fb0915de6ce5d76033159af3e7056676a02d265d
Matt Sturgeon <matt@sturgeon.me.uk>
2024-02-13 17:56:34 +0000

    Test File.asset extension function

--- src/test/kotlin/MinecraftAssetIndexTest.kt
+++ src/test/kotlin/MinecraftAssetIndexTest.kt
@@ -1,7 +1,10 @@
 import dev.mattsturgeon.assets.MinecraftAssetIndex
+import dev.mattsturgeon.assets.asset
 import kotlinx.serialization.json.Json
 import org.intellij.lang.annotations.Language
+import java.io.File
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.asserter
 
 class MinecraftAssetIndexTest {
@@ -33,5 +36,19 @@ class MinecraftAssetIndexTest {
         val afZa = index.objects["minecraft/lang/af_za.json"]
         asserter.assertNotNull("Has af_za", afZa)
         asserter.assertEquals("Has af_za hash", "d6ecd3c70e0259c6b4246807c4b49efb80bd8aac", afZa?.hash)
+    }
+
+    @Test
+    fun `Correctly resolve object`() {
+        val base = File("foo")
+        mapOf(
+            "d6ecd3c70e0259c6b4246807c4b49efb80bd8aac" to "objects/d6/d6ecd3c70e0259c6b4246807c4b49efb80bd8aac",
+            "96f1185dd550fd3756c80246114263cad64c8b78" to "objects/96/96f1185dd550fd3756c80246114263cad64c8b78",
+            "24cb6024c4bf04910cc2325f692f31692560c04f" to "objects/24/24cb6024c4bf04910cc2325f692f31692560c04f",
+            "invalid" to "objects/in/invalid",
+            "0_[]asdfpoijadsg34qt2redvp9sh£%QT£GVs" to "objects/0_/0_[]asdfpoijadsg34qt2redvp9sh£%QT£GVs"
+        ).forEach { (hash, expected) ->
+            assertEquals(base.resolve(expected), base.asset(hash))
+        }
     }
 }
