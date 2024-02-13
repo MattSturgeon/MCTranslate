b2b2b3e6358d4fe804031197f72072b76762673d
Matt Sturgeon <matt@sturgeon.me.uk>
2024-02-13 14:43:20 +0000

    Add some IndexedAssets integration tests
    Using DummyIndexedAssets

---
+++ src/test/kotlin/IndexedAssetsTest.kt
@@ -0,0 +1,87 @@
+import dev.mattsturgeon.assets.Assets
+import dev.mattsturgeon.assets.DummyAssets
+import org.intellij.lang.annotations.Language
+import kotlin.test.*
+
+class IndexedAssetsTest {
+
+    private lateinit var assetsWithPackMeta: Assets
+    private lateinit var assetsWithoutPackMeta: Assets
+
+    @BeforeTest
+    fun setup() {
+        assetsWithPackMeta = DummyAssets(mapOf<String, @receiver:Language("JSON") String>(
+            "pack.mcmeta" to """
+                        {
+                          "languages": {
+                            "en_us": {
+                              "name": "English",
+                              "region": "US",
+                              "bidirectional": false
+                            }
+                          }
+                        }
+                    """.trimIndent(),
+            "minecraft/lang/en_us.json" to """
+                        {
+                          "some.key": "some value"
+                        }
+                    """.trimIndent()
+        ))
+
+        assetsWithoutPackMeta = DummyAssets(mapOf<String, @receiver:Language("JSON") String>(
+            "minecraft/lang/en_us.json" to """
+                        {
+                          "some.key": "some value"
+                        }
+                    """.trimIndent()
+        ))
+    }
+
+    @Test
+    fun `Finds pack mcmeta`() {
+        assertNotNull(assetsWithPackMeta.packMeta(), "Has pack.mcmeta")
+        assertNull(assetsWithoutPackMeta.packMeta(), "Doesn't have pack.mcmeta")
+    }
+
+    @Test
+    fun `Pack mcmeta has correct entries`() {
+        val languages = assetsWithPackMeta.packMeta()?.languages!!
+        assertContains(languages, "en_us")
+        assertEquals(languages["en_us"]?.name, "English")
+        assertEquals(languages["en_us"]?.region, "US")
+        assertEquals(languages["en_us"]?.bidirectional, false)
+    }
+
+    @Test
+    fun `Doesn't find missing translations`() {
+        sequenceOf(
+            "en_gb",
+            "t_it"
+        ).forEach {
+            assertNull(assetsWithPackMeta.getLang(it))
+        }
+    }
+
+    @Test
+    fun `Fails gracefully for invalid lang code`() {
+        sequenceOf(
+            "adsflshasdfouihasdf",
+            "241liugwefs__43324kjn-asfkjn$.,easf''\"soaid6^5!£"
+        ).forEach {
+            assertNull(assetsWithPackMeta.getLang(it))
+        }
+    }
+
+    @Test
+    fun `Finds translations`() {
+        val translations = assetsWithPackMeta.getLang("en_us")
+        assertNotNull(translations)
+    }
+
+    @Test
+    fun `Finds correct translations`() {
+        val translations = assetsWithPackMeta.getLang("en_us")!!
+        assertEquals("some value", translations["some.key"])
+    }
+}
